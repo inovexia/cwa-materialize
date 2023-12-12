b10651449f3956ff56538efcf1b29cd4cca796ee
Asif Aziz <asifaziz01@users.noreply.github.com>
2023-12-12 16:20:45 +0530

    Create test drawer in-progress

---
+++ src/pages/tests/_views/create.js
@@ -0,0 +1,207 @@
+import Grid from '@mui/material/Grid'
+import Select from '@mui/material/Select'
+import Button from '@mui/material/Button'
+import MenuItem from '@mui/material/MenuItem'
+import TextField from '@mui/material/TextField'
+import InputLabel from '@mui/material/InputLabel'
+import FormControl from '@mui/material/FormControl'
+import FormHelperText from '@mui/material/FormHelperText'
+import CircularProgress from '@mui/material/CircularProgress'
+
+// ** Third Party Imports
+import * as yup from 'yup'
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const defaultValues = {
+  title: '',
+  type: 'evaluated',
+  details: '',
+  category_guid: 'quiz'
+}
+
+const showErrors = (field, valueLen, min) => {
+  if (valueLen === 0) {
+    return `${field} field is required`
+  } else if (valueLen > 0 && valueLen < min) {
+    return `${field} must be at least ${min} characters`
+  } else {
+    return ''
+  }
+}
+
+const schema = yup.object().shape({
+  title: yup.string().required().min(3),
+  type: yup.string().required(),
+  details: yup.string(),
+  category: yup.string()
+})
+
+
+
+const CreateTestForm = props => {
+
+  const { onSubmit, isLoading, toggle, categories } = props
+
+  // ** Hooks
+  const {
+    reset,
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    defaultValues,
+    mode: 'onChange',
+    resolver: yupResolver(schema)
+  })
+
+  const handleClose = () => {
+    toggle()
+    reset()
+  }
+
+  console.log(categories)
+
+  return (
+
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={5}>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <Controller
+              name='title'
+              control={control}
+              rules={{ required: true }}
+              render={({ field: { value, onChange } }) => (
+                <TextField
+                  value={value}
+                  label='Test Name'
+                  onChange={onChange}
+                  placeholder='Test name'
+                  error={Boolean(errors.title)}
+                  aria-describedby='validation-schema-title'
+                />
+              )}
+            />
+            {errors.title && (
+              <FormHelperText sx={{ color: 'error.main' }} id='validation-schema-title'>
+                {errors.title.message}
+              </FormHelperText>
+            )}
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={12}>
+          <FormControl fullWidth sx={{ mb: 6 }}>
+            <InputLabel id='test-type'>Test Type</InputLabel>
+            <Controller
+              name='type'
+              control={control}
+              rules={{ required: true }}
+              render={({ field: { value, onChange } }) => (
+                <Select
+                  fullWidth
+                  value={value || ''}
+                  defaultValue=''
+                  label='Test Type'
+                  labelId='typeLabel'
+                  onChange={onChange}
+                  error={Boolean(errors.type)}
+                  inputProps={{ placeholder: 'Select Type' }}
+                >
+                  <MenuItem value='evaluated'>Evaluated</MenuItem>
+                  <MenuItem value='practice'>Practice</MenuItem>
+                  <MenuItem value='quiz'>Quiz</MenuItem>
+                </Select>
+              )}
+            />
+            {errors.type && (
+              <FormHelperText sx={{ color: 'error.main' }} id='validation-test-type'>
+                {errors.type.message}
+              </FormHelperText>
+            )}
+          </FormControl>
+        </Grid>
+
+
+        <Grid item xs={12}>
+          <FormControl fullWidth sx={{ mb: 6 }}>
+            <Controller
+              name='details'
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextField
+                  rows={4}
+                  multiline
+                  {...field}
+                  label='Details'
+                  error={Boolean(errors.details)}
+                  aria-describedby='validation-test-details'
+                />
+              )}
+            />
+            {errors.details && (
+              <FormHelperText sx={{ color: 'error.main' }} id='validation-test-details'>
+                {errors.details.message}
+              </FormHelperText>
+            )}
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={12}>
+          <FormControl fullWidth sx={{ mb: 6 }}>
+            <InputLabel id='test-category'>Category</InputLabel>
+            <Controller
+              name='category_guid'
+              control={control}
+              rules={{ required: true }}
+              render={({ field: { value, onChange } }) => (
+                <Select
+                  fullWidth
+                  value={categories || ''}
+                  defaultValue=''
+                  label='Category'
+                  labelId='categoryLabel'
+                  onChange={onChange}
+                  error={Boolean(errors.category_guid)}
+                  inputProps={{ placeholder: 'Select Type' }}
+                >
+                  {categories && categories.map((key, row) => {
+                    <MenuItem value={row.guid} key={key}>{row.title}</MenuItem>
+                  })}
+                </Select>
+              )}
+            />
+            {errors.category_guid && (
+              <FormHelperText sx={{ color: 'error.main' }} id='validation-basic-select'>
+                {errors.category_guid.message}
+              </FormHelperText>
+            )}
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center' }}>
+          <Button size='large' type='submit' variant='contained' disabled={isLoading ? true : false}>
+            {isLoading ? (
+              <CircularProgress
+                sx={{
+                  color: 'common.white',
+                  width: '20px !important',
+                  height: '20px !important',
+                  mr: theme => theme.spacing(2)
+                }}
+              />
+            ) : null}
+            Submit
+          </Button>
+          <Button size='large' variant='outlined' color='secondary' onClick={handleClose}>
+            Cancel
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
+  )
+}
+
+export default CreateTestForm
--- src/pages/tests/create.js
+++ src/pages/tests/create.js
@@ -1,36 +1,25 @@
 // ** React Imports
-import { useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 
 // ** MUI Imports
 import Drawer from '@mui/material/Drawer'
-import Select from '@mui/material/Select'
-import Button from '@mui/material/Button'
-import MenuItem from '@mui/material/MenuItem'
 import { styled } from '@mui/material/styles'
-import TextField from '@mui/material/TextField'
 import IconButton from '@mui/material/IconButton'
-import InputLabel from '@mui/material/InputLabel'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
-import FormControl from '@mui/material/FormControl'
-import FormHelperText from '@mui/material/FormHelperText'
-import CircularProgress from '@mui/material/CircularProgress'
 import Card from '@mui/material/Card'
-import Grid from '@mui/material/Grid'
-import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
 import toast from 'react-hot-toast'
-
-// ** Third Party Imports
-import * as yup from 'yup'
-import { useForm, Controller } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
+import FormHelperText from '@mui/material/FormHelperText'
 
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
 
+// ** Icon Imports
+import CreateTestForm from 'src/pages/tests/_views/create'
+
 // ** Actions Imports
-import { AddTest } from 'src/pages/tests/_models/TestModel'
+import { GetCategories } from 'src/pages/tests/_models/TestModel'
 
 const Header = styled(Box)(({ theme }) => ({
   display: 'flex',
@@ -39,51 +28,31 @@ const Header = styled(Box)(({ theme }) => ({
   justifyContent: 'space-between'
 }))
 
-const defaultValues = {
-  title: '',
-  type: '',
-  details: '',
-  category: ''
-}
-
-const showErrors = (field, valueLen, min) => {
-  if (valueLen === 0) {
-    return `${field} field is required`
-  } else if (valueLen > 0 && valueLen < min) {
-    return `${field} must be at least ${min} characters`
-  } else {
-    return ''
-  }
-}
-
-const schema = yup.object().shape({
-  title: yup.string().required().min(3),
-  type: yup.string().required(),
-  details: yup.string(),
-  category: yup.string()
-})
-
 
 const SidebarAddTest = props => {
   // ** Props
   const { open, toggle } = props
 
   // ** State
-  const [type, setType] = useState('evaluated')
+  const [responseMessage, setResponseMessage] = useState('')
   const [isLoading, setLoading] = useState(false)
-  const [responseMessage, setResponseMessage] = useState(false)
+  const [categories, setCategories] = useState([])
 
-  // ** Hooks
-  const {
-    reset,
-    control,
-    handleSubmit,
-    formState: { errors }
-  } = useForm({
-    defaultValues,
-    mode: 'onChange',
-    resolver: yupResolver(schema)
-  })
+  const getCategories = useCallback(async () => {
+    const response = await GetCategories()
+
+    return response
+  }, [])
+
+  useEffect(() => {
+    getCategories()
+    .then((response) => {
+      if (response.success) {
+        setCategories(response.payload)
+      }
+    })
+  }, [getCategories])
+
 
   const onSubmit = async (data) => {
     setLoading (true)
@@ -97,24 +66,18 @@ const SidebarAddTest = props => {
     }
   }
 
-
-  const handleClose = () => {
-    toggle()
-    reset()
-  }
-
   return (
     <Drawer
       open={open}
       anchor='right'
       variant='temporary'
-      onClose={handleClose}
+      onClose={toggle}
       ModalProps={{ keepMounted: true }}
       sx={{ '& .MuiDrawer-paper': { width: { xs: 300, sm: 400 } } }}
     >
       <Header>
         <Typography variant='h6'>Add Test</Typography>
-        <IconButton size='small' onClick={handleClose} sx={{ color: 'text.primary' }}>
+        <IconButton size='small' onClick={toggle} sx={{ color: 'text.primary' }}>
           <Icon icon='mdi:close' fontSize={20} />
         </IconButton>
       </Header>
@@ -125,137 +88,12 @@ const SidebarAddTest = props => {
       </Box>
       <Card>
         <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Grid container spacing={5}>
-              <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <Controller
-                    name='title'
-                    control={control}
-                    rules={{ required: true }}
-                    render={({ field: { value, onChange } }) => (
-                      <TextField
-                        value={value}
-                        label='First Name'
-                        onChange={onChange}
-                        placeholder='Test title'
-                        error={Boolean(errors.title)}
-                        aria-describedby='validation-schema-title'
-                      />
-                    )}
-                  />
-                  {errors.title && (
-                    <FormHelperText sx={{ color: 'error.main' }} id='validation-schema-title'>
-                      {errors.title.message}
-                    </FormHelperText>
-                  )}
-                </FormControl>
-              </Grid>
-
-              <Grid item xs={12}>
-                <FormControl fullWidth sx={{ mb: 6 }}>
-                  <InputLabel id='test-type'>Test Type</InputLabel>
-                  <Controller
-                    name='type'
-                    control={control}
-                    rules={{ required: true }}
-                    render={({ field: { value, onChange } }) => (
-                      <Select
-                        fullWidth
-                        value={value}
-                        label='Test Type'
-                        labelId='testTypeLabel'
-                        onChange={onChange}
-                        error={Boolean(errors.type)}
-                        inputProps={{ placeholder: 'Select Type' }}
-                      >
-                        <MenuItem value='evaluated'>Evaluated</MenuItem>
-                        <MenuItem value='practice'>Practice</MenuItem>
-                        <MenuItem value='quiz'>Quiz</MenuItem>
-                      </Select>
-                    )}
-                  />
-                  {errors.type && (
-                    <FormHelperText sx={{ color: 'error.main' }} id='validation-basic-select'>
-                      This field is required
-                    </FormHelperText>
-                  )}
-                </FormControl>
-              </Grid>
-
-              <Grid item xs={12}>
-                <FormControl fullWidth sx={{ mb: 6 }}>
-                  <Controller
-                    name='details'
-                    control={control}
-                    rules={{ required: true }}
-                    render={({ field }) => (
-                      <TextField
-                        rows={4}
-                        multiline
-                        {...field}
-                        label='Bio'
-                        error={Boolean(errors.details)}
-                        aria-describedby='validation-basic-textarea'
-                      />
-                    )}
-                  />
-                  {errors.details && (
-                    <FormHelperText sx={{ color: 'error.main' }} id='validation-basic-textarea'>
-                      This field is required
-                    </FormHelperText>
-                  )}
-                </FormControl>
-              </Grid>
-
-              <Grid item xs={12}>
-                <FormControl fullWidth sx={{ mb: 6 }}>
-                  <InputLabel id='category'>Test Type</InputLabel>
-                  <Controller
-                    name='category'
-                    control={control}
-                    rules={{ required: true }}
-                    render={({ field: { value, onChange } }) => (
-                      <Select
-                        fullWidth
-                        value={value}
-                        label='Category'
-                        labelId='categoryLabel'
-                        onChange={onChange}
-                        error={Boolean(errors.category)}
-                        inputProps={{ placeholder: 'Select Category' }}
-                      >
-                        <MenuItem value='evaluated'>Evaluated</MenuItem>
-                        <MenuItem value='practice'>Practice</MenuItem>
-                        <MenuItem value='quiz'>Quiz</MenuItem>
-                      </Select>
-                    )}
-                  />
-                  {errors.category && (
-                    <FormHelperText sx={{ color: 'error.main' }} id='validation-basic-select'>
-                      This field is required
-                    </FormHelperText>
-                  )}
-                </FormControl>
-              </Grid>
-
-              <Grid item xs={12}>
-                <Button size='large' type='submit' variant='contained' disabled={isLoading ? true : false}>
-                  {isLoading ? (
-                    <CircularProgress
-                      sx={{
-                        color: 'common.white',
-                        width: '20px !important',
-                        height: '20px !important',
-                        mr: theme => theme.spacing(2)
-                      }}
-                    />
-                  ) : null}
-                  Submit
-                </Button>
-              </Grid>
-            </Grid>
-          </form>
+          <CreateTestForm
+            onSubmit={onSubmit}
+            isLoading={isLoading}
+            toggle={toggle}
+            categories={categories}
+          />
         </CardContent>
       </Card>
 
